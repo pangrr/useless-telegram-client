fd0b3b258adcfa5c503caf16f16f4d47c3896fea
prpr <prpr@123.com>
2024-05-05 13:15:20 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
-  <Splitter v-if="user" style="height: 100vh; width: 100vw;">
-    <SplitterPanel class="flex align-items-center justify-content-center" :size="25" :minSize="10">dialogs
-    </SplitterPanel>
-    <SplitterPanel class="flex align-items-center justify-content-center" :size="75"> Panel 2 </SplitterPanel>
-  </Splitter>
-  <div v-else class='flex justify-content-center' style="height: 100vh; width: 100vw;">
-    <div class='flex flex-column gap-2 justify-content-center'>
-      <label for='phone'>Phone Number</label>
-      <InputText id='phone' type='text' v-model='phone' />
-      <Button label='Send Code' @click='sendCode' />
-      <label for='code'>Code</label>
-      <InputText id='code' v-model='code' />
-      <Button label='Login' @click='login' />
+  <div style='height: 100vh; width: 100vw;'>
+    <SpeedDial v-if='loggedIn === true' :model='menuItems' direction='down' style='right: 1rem; top: 2rem;' />
+    <Splitter v-if='loggedIn === true'>
+      <SplitterPanel :size='25' :minSize='10'>
+        <Listbox v-model='selectedDialog' :options='dialogs' optionLabel='name' class='w-full' listStyle='height:100vh'>
+          <template #option='slotProps'>
+            <div class='flex align-items-center h-5rem'>
+              <img src='' class='mr-2' style='width: 18px' />
+              <div>{{ slotProps.option.name }}</div>
+            </div>
+          </template>
+        </Listbox>
+      </SplitterPanel>
+      <SplitterPanel class='flex align-items-center justify-content-center' :size='75'> Panel 2 </SplitterPanel>
+    </Splitter>
+    <div v-else-if='loggedIn === false' style='height: 100vh; width: 100vw;'
+      class='flex justify-content-center flex-grow-1'>
+      <div class='flex flex-column gap-2 justify-content-center'>
+        <label for='phone'>Phone Number</label>
+        <InputText id='phone' type='text' v-model='phone' />
+        <Button label='Send Code' @click='sendCode' />
+        <label for='code'>Code</label>
+        <InputText id='code' v-model='code' />
+        <Button label='Login' @click='login' />
+      </div>
     </div>
   </div>
 </template>
@@ -28,13 +40,17 @@ const client = new TelegramClient(session, authCredentials.apiId, authCredential
 
 const phone = ref('+8618716398744')
 const code = ref()
+const loggedIn = ref()
 const user = ref()
 const dialogs = ref([])
+const selectedDialog = ref()
 
 onMounted(async () => {
   await client.connect()
   if (await client.checkAuthorization()) {
-    await getDataAfterLoggedIn()
+    await onLoggedIn()
+  } else {
+    loggedIn.value = false
   }
 })
 
@@ -46,17 +62,34 @@ async function login() {
   try {
     await client.signInUser(authCredentials, { phoneNumber: phone.value, phoneCode: code.value })
     localStorage.setItem('session', JSON.stringify(client.session.save()))
-    await getDataAfterLoggedIn()
+    await onLoggedIn()
   } catch (e) {
     console.error(e)
   }
 }
 
-async function getDataAfterLoggedIn() {
-  user.value = await client.getMe()
-  dialogs.value = await client.getDialogs()
-  console.log(dialogs.value)
+function logout() {
+  localStorage.removeItem('session')
+  user.value = null
+  loggedIn.value = false
 }
+
+async function onLoggedIn() {
+  user.value = await client.getMe()
+  console.log('user', user.value)
+  loggedIn.value = true
+  dialogs.value = await client.getDialogs()
+  console.log('dialogs', dialogs.value)
+}
+
+const menuItems = ref([
+  {
+    label: 'Logout',
+    icon: 'pi pi-sign-out',
+    command: logout
+  }
+])
+
 </script>
 
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import "primeflex/primeflex.css"
-import "primeicons/primeicons.css"
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
 import 'primevue/resources/themes/aura-dark-green/theme.css'
 import PrimeVue from 'primevue/config'
 import Button from 'primevue/button'
@@ -10,6 +10,8 @@ import InputText from 'primevue/inputtext'
 import InputNumber from 'primevue/inputnumber'
 import Splitter from 'primevue/splitter'
 import SplitterPanel from 'primevue/splitterpanel'
+import SpeedDial from 'primevue/speeddial'
+import Listbox from 'primevue/listbox'
 
 const app = createApp(App)
 
@@ -19,5 +21,7 @@ app.component('InputText', InputText)
 app.component('InputNumber', InputNumber)
 app.component('Splitter', Splitter)
 app.component('SplitterPanel', SplitterPanel)
+app.component('SpeedDial', SpeedDial)
+app.component('Listbox', Listbox)
 
 app.mount('#app')
